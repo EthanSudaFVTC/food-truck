3949d5cda18068945621be8187b61c28e0549add
Ethan Suda <4ethans@gmail.com>
2024-12-14 20:55:06 -0600

    added functionality for the event half of the admin page

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,20 +1,39 @@
 // add new menu items
 (async () => {
-document.getElementById('add-menu-item').addEventListener('submit', async (e) => {
-    e.preventDefault() // prevents page from not showing error
-    const newMenuItem = {
-        name: document.getElementById('menu-item-name').value, 
-        description: document.getElementById('menu-item-description').value, 
-        price: document.getElementById('menu-item-price').value, 
-        url: document.getElementById('menu-item-url').value
-    }
-    fetch('api/v1/food-truck/menu', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newMenuItem)
+    // menu
+    document.getElementById('add-menu-item').addEventListener('submit', async (e) => {
+        e.preventDefault() // prevents page from not showing error
+        const newMenuItem = {
+            name: document.getElementById('menu-item-name').value, 
+            description: document.getElementById('menu-item-description').value, 
+            price: document.getElementById('menu-item-price').value, 
+            url: document.getElementById('menu-item-url').value
+        }
+        fetch('api/v1/food-truck/menu', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newMenuItem)
+        })
     })
-})
+
+    // event
+    document.getElementById('add-event').addEventListener('submit', async (e) => {
+        e.preventDefault() // prevents page from not showing error
+        const newEvent = {
+            name: document.getElementById('event-name').value, 
+            description: document.getElementById('event-location').value, 
+            price: document.getElementById('event-date').value, 
+            url: document.getElementById('event-time').value
+        }
+        fetch('api/v1/food-truck/events', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newEvent)
+        })
+    })
 
 })()
